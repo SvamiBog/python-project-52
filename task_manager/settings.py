9d29f0a982b173c97cb31e78ac595f124b25e1be
SvamiBog <rubanezmaxim@gmailcom>
2024-09-15 14:50:27 +0200

    fix host allowed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,13 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "webserver",
-    '127.0.0.1',
-    '0.0.0.0',
-    'localhost',
-    'python-project-52-37sg.onrender.com',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
